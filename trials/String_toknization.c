eaf632e0fc97072e0930678674f6bd012429e204
Tinsuag <tinsaeareg.work@gmail.com>
2023-09-22 20:25:39 +0300

    string delimiter function

---
+++ trials/String_toknization.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define BUFLEN 100
+
+int main( int argc, char **argv)
+{
+	char input[BUFLEN] = "Rey, Luke, Leia: Anakin, Padme";
+	int i = 0;
+
+	char *token = strtok(input, "-");
+	printf("%d:[%p] --- %s\n", i, token, token);
+
+}
